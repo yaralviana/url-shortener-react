cf0d438182627ceffd21984822cf29bc6f48ec17
yara <yaralviana@gmail.com>
2023-02-08 23:14:50 -0300

    Insere logo como link

--- url-shortener/src/components/Logo/index.js
+++ url-shortener/src/components/Logo/index.js
@@ -1,3 +1,10 @@
+import "./logo.css"
+import { Link } from "react-router-dom"
+
 export function Logo() {
-  return <h1>URLize</h1>
+  return (
+    <Link to="/">
+      <h1 className="logo">URLize</h1>
+    </Link>
+  )
 }
--- url-shortener/src/pages/Error/index.js
+++ url-shortener/src/pages/Error/index.js
@@ -1,9 +1,11 @@
 import "./error.css"
 import { Link } from "react-router-dom"
+import { Logo } from "../../components/Logo"
 
 export default function Admin() {
   return (
     <div className="error">
+      <Logo />
       <h1>Página não encontrada</h1>
       <p>Essa página que você está procurando não existe.</p>
       <Link className="link" to="/">
